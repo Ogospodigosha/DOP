2763454b56e70f129726bb3b206be487a9eac5cf
Ogospodigosha <101788284+Ogospodigosha@users.noreply.github.com>
2022-08-03 18:57:04 +0300

    add first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {Button} from "./Components/Button";
+type getType ={
+    "userId": number
+    "id": number
+    "title": string
+    "body":string
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const[get, setGet] =useState<Array<getType>>([])
+    console.log(get)
+    const getRequestHandler = () => {
+        setGet([])
+    }
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/posts/')
+            .then(response => response.json())
+            .then(json => setGet(json))
+    }, [])
+    return (
+        <div className="App">
+            <Button callback={getRequestHandler} nickName="Cleanpage"/>
+            <p></p>
+            <ul>
+            {get.map((el)=>{
+                return (<div>
+                  <li><span>{el.id}</span><span>{el.userId}</span><span>{el.title}</span></li>
+
+                    </div>
+                )
+            })}
+        </ul>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+type ButtonPropsType = {
+    nickName: string
+    callback: ()=>void
+}
+export const Button = (props:ButtonPropsType) => {
+    const onClickHandler =()=>{
+        props.callback()
+    }
+    return (
+       <button onClick={onClickHandler}>{props.nickName}</button>
+    );
+};
+
